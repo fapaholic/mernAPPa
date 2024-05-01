a4afeee99b6d5bda7b613bbb0e42a1511d03420e
fapaholic <jaspervincem.99@gmail.com>
2024-05-01 14:14:32 +0800

    mana ang backend

---
+++ backend/models/studentsModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const StudentsSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+    },
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    mname: {
+        type:String,
+        required: false
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    bday: {
+        type: String,
+        required: true,
+    },
+    level: {
+        type: String,
+        required: true
+    },
+    payable: {
+        type: Number,
+        default: 250,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    usertype: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    }
+    
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', StudentsSchema)
